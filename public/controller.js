4401396126d03764e612fcd7a1f4e15f22029c86
Curtis Allen <curtis.n.allen@gmail.com>
2013-01-15 22:39:15 -0700

    use the service

--- public/controller.js
+++ public/controller.js
@@ -1,18 +1,20 @@
 
 function NodewsosCtrl($scope, $log, $http, $injector) {
-	var map = $injector.get('az.services.layers');
+	var mapService = $injector.get('az.services.layers');
+
 
 	$scope.findPark = function() {
-		$log.log("I see this map " + map.getMapLayers());
+		$log.log("I see this mapLayers " + mapService.getMapLayers());
 		
 	}
 
 	$scope.mapClick = function(e) {
 		$log.log("Map click event at " + e.latlng);
+
 		$http.get('/ws/parks/near?lat=' + e.latlng.lat + '&lon=' + e.latlng.lng)
 			.success(function(response) {
 				$log.log("Got this response: " + angular.toJson(response));
-				L.marker([-105.68962, 40.414]).addTo(this);
+				L.marker([-105.68962, 40.414]).addTo(mapService.getMap());
 			});
 	}
 }
