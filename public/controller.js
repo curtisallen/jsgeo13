f6f63c361388f83be25f3def38b5385686bf4dc6
Curtis Allen <curtis.n.allen@gmail.com>
2013-01-15 22:56:06 -0700

    fix corrds

--- public/controller.js
+++ public/controller.js
@@ -13,8 +13,9 @@ function NodewsosCtrl($scope, $log, $http, $injector) {
 
 		$http.get('/ws/parks/near?lat=' + e.latlng.lat + '&lon=' + e.latlng.lng)
 			.success(function(response) {
-				$log.log("Got this response: " + angular.toJson(response));
+				//$log.log("Got this response: " + angular.toJson(response));
 				angular.forEach(response, function(park) {
+					console.log(park.pos);
 					L.marker(park.pos).addTo($scope.map);
 				});
 				//L.marker([-105.68962, 40.414]).addTo($scope.map);
